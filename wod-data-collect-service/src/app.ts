8507a1c1368d91f14e3d6d5fa2a0dece27b28b65
Yavor Mihaylov <myavor@vmware.com>
2023-02-18 13:39:11 +0200

    Add docker-compose.yaml, connect node.js app to postgresql

--- wod-data-collect-service/src/app.ts
+++ wod-data-collect-service/src/app.ts
@@ -1,5 +1,7 @@
 import { config } from 'dotenv';
 import express, { Application, NextFunction, Request, Response } from 'express';
+import { Client } from 'pg';
+
 
 config();
 
@@ -7,12 +9,31 @@ const app: Application = express();
 app.use(express.json());
 
 app.get('/', (request: Request, response: Response, next: NextFunction) => {
+    tryToConnectToDatabase();
     response.json({
         status: "Successs",
         message: "Hello World!",
         description: "Express server with TypeScript"
     });
 });
+
+async function tryToConnectToDatabase() {
+    const client = new Client({
+        user: 'postgres',
+        password: 'postgres',
+        host: 'crossfit-diary-db-service', // This is the name of the Postgres service defined in your docker-compose.yaml
+        database: 'crossfit_diary',
+        port: 5432
+      });
+
+      try {
+        await client.connect();
+        console.log('Connected to the database');
+      } catch (err) {
+        console.error('Failed to connect to the database');
+        console.error(err);
+      }
+}
 
 app.post('/collect-wod-data', (request: Request, response: Response, next: NextFunction) => {
     request.body.text;
