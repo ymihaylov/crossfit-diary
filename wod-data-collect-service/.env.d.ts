797f6cc25a18f55c10c9c5884d6afe42f0766586
Yavor Mihaylov <myavor@vmware.com>
2023-02-19 13:49:58 +0200

    Add Sequelize to the project and do some changes on .env variables

---
+++ wod-data-collect-service/.env.d.ts
@@ -0,0 +1,11 @@
+declare namespace NodeJS {
+	interface ProcessEnv {
+		APP_PORT: number;
+
+		DB_HOST: string;
+		DB_PORT: number;
+		DB_USER: string;
+		DB_PASSWORD: string;
+		DB_NAME: string;
+	}
+}
--- wod-data-collect-service/src/app.ts
+++ wod-data-collect-service/src/app.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import express, { Application, NextFunction, Request, Response } from 'express';
 import { Client } from 'pg';
-
+import { Sequelize } from 'sequelize';
 
 config();
 
@@ -17,31 +17,49 @@ app.get('/', (request: Request, response: Response, next: NextFunction) => {
     });
 });
 
-async function tryToConnectToDatabase() {
+async function tryToConnectToDatabaseOld() {
     const client = new Client({
         user: 'postgres',
         password: 'postgres',
-        host: 'crossfit-diary-db-service', // This is the name of the Postgres service defined in your docker-compose.yaml
+        host: 'crossfit-diary-db-service',
         database: 'crossfit_diary',
         port: 5432
       });
 
       try {
         await client.connect();
-        console.log('Connected to the database');
+        console.log('Connected to the databas');
       } catch (err) {
         console.error('Failed to connect to the database');
         console.error(err);
       }
 }
 
+async function tryToConnectToDatabase() {
+    const sequelize = new Sequelize({
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        port: Number(process.env.DB_PORT),
+      });
+
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+
 app.post('/collect-wod-data', (request: Request, response: Response, next: NextFunction) => {
     request.body.text;
     response.send('Yyyyss POST Request! Express server with TypeScript!');
 });
 
-const PORT = process.env.PORT;
+const APP_PORT = process.env.APP_PORT;
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+app.listen(APP_PORT, () => {
+    console.log(`Server is listening on port ${APP_PORT}`);
 });
