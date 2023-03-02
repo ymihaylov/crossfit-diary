9a02747e3cbc4e4a820ffca33044ffacdd091abe
Yavor Mihaylov <myavor@vmware.com>
2023-03-02 15:33:29 +0200

    Add taiwind to the project

--- crossfit-diary-frontend/src/pages/index.tsx
+++ crossfit-diary-frontend/src/pages/index.tsx
@@ -3,21 +3,27 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>CrossFit Diary - Workouts</title>
-        <meta name="description" content="CrossFit Diary - Workouts" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      Here should be the list of workouts
-
-      <ul>
-        <li><Link href="/workout">Workout 1</Link></li>
-        <li><Link href="/workout">Workout 2</Link></li>
-        <li><Link href="/workout">Workout 3</Link></li>
-      </ul>
-    </>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
+
+  // return (
+  //   <>
+  //     <Head>
+  //       <title>CrossFit Diary - Workouts</title>
+  //       <meta name="description" content="CrossFit Diary - Workouts" />
+  //       <meta name="viewport" content="width=device-width, initial-scale=1" />
+  //       <link rel="icon" href="/favicon.ico" />
+  //     </Head>
+
+  //     Here should be the list of workouts
+
+  //     <ul>
+  //       <li><Link href="/workout">Workout 1</Link></li>
+  //       <li><Link href="/workout">Workout 2</Link></li>
+  //       <li><Link href="/workout">Workout 3</Link></li>
+  //     </ul>
+  //   </>
+  // )
 }
