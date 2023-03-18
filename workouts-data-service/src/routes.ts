babefef6b11b722a531e29186984da94f58c9a04
Yavor Mihaylov <myavor@vmware.com>
2023-03-18 11:28:49 +0200

    Add workout validation on create workout request

--- workouts-data-service/src/routes.ts
+++ workouts-data-service/src/routes.ts
@@ -3,10 +3,15 @@ import { Router } from "express";
 import IndexController from "./controllers/IndexController";
 
 import WorkoutsController from "./controllers/WorkoutsController";
+import CreateWorkoutValidator from "./validators/CreateWorkoutValidator";
 
 const routes: Router = Router()
 
 routes.get('/', IndexController.index);
-routes.post('/workouts/create', WorkoutsController.createWorkout);
+routes.post(
+	'/workouts/create',
+	CreateWorkoutValidator.validateWorkoutCreation,
+	WorkoutsController.createWorkout
+);
 
 export default routes;
---
+++ workouts-data-service/src/validators/CreateWorkoutValidator.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express";
+import { check, validationResult } from "express-validator";
+
+class CreateWorkoutValidator {
+	public validateWorkoutCreation = [
+		check('text', 'Text is required').notEmpty(),
+
+		check('date', 'Invalid date format')
+			.optional()
+			.custom(this.validateDate),
+
+		this.handleValidationResult,
+	];
+
+	private handleValidationResult(request: Request, response: Response, next: NextFunction) {
+		const errors = validationResult(request);
+
+		if (!errors.isEmpty()) {
+			return response
+				.status(400)
+				.json({ errors: errors.array() });
+		}
+
+		next();
+	}
+
+	private validateDate(value: string) {
+		const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+
+		if (!dateRegex.test(value)) {
+          throw new Error('Invalid date format. Correct format is YYYY-MM-DD');
+        }
+
+		if (isNaN(Date.parse(value))) {
+          throw new Error('Invalid date');
+        }
+
+		return true;
+	}
+}
+
+export default new CreateWorkoutValidator;
